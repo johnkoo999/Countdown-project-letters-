114d15376a2199eeb399733180c23e614c068d58
johnkoo999 <johnkoo999@gmail.com>
2014-11-12 21:50:04 +0800

    Create glos.h

---
+++ glos.h
@@ -0,0 +1,29 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+string line;
+bool glosans;
+
+void intxt(string ans) {
+	glosans=0;
+	ifstream myfile;
+	myfile.open ("dictionary");
+	if (myfile.is_open()){
+	while (! myfile.eof() )    {
+		getline (myfile,line);
+		if (ans==line) {
+			glosans=1;
+			cout << "Your answer is found in the glossary.\n";
+			myfile.close();
+		}
+		else {
+			continue;
+		}
+	}
+    myfile.close();
+}
+}
+
